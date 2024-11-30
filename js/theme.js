5ab43aa65442e6e4578cd68f1dff84b51174b31e
FILÂTRE Killian <k.filatre@gmail.com>
2024-11-30 16:27:20 +0100

    init project portfolio

---
+++ js/theme.js
@@ -0,0 +1,106 @@
+$('.page-scroll').bind('click', function(event) {
+    var $anchor = $(this);
+    $('html, body').stop().animate({
+        scrollTop: $($anchor.attr('href')).offset().top -64
+    }, 1500, 'easeInOutExpo');
+    event.preventDefault();
+});
+
+var cbpAnimatedHeader = (function() {
+
+    var docElem = document.documentElement,
+        header = document.querySelector( '.navbar-fixed-top' ),
+        didScroll = false,
+        changeHeaderOn = 10;
+
+    function init() {
+        window.addEventListener( 'scroll', function( event ) {
+            if( !didScroll ) {
+                didScroll = true;
+                setTimeout( scrollPage, 250 );
+            }
+        }, false );
+    }
+
+    function scrollPage() {
+        var sy = scrollY();
+        if ( sy >= changeHeaderOn ) {
+            classie.add( header, 'navbar-shrink' );
+        }
+        else {
+            classie.remove( header, 'navbar-shrink' );
+        }
+        didScroll = false;
+    }
+
+    function scrollY() {
+        return window.pageYOffset || docElem.scrollTop;
+    }
+
+    init();
+
+})();
+
+$('body').scrollspy({
+    target: '.navbar',
+    offset: 65
+})
+
+// Wait for window load
+$(window).load(function() {
+    // Animate loader off screen
+    $(".page-loader").fadeOut("slow");
+});
+
+$.stellar({
+    horizontalScrolling: false,
+    verticalScrolling: true,
+});
+
+new WOW().init();
+
+$('.counter').counterUp({
+    delay: 10,
+    time: 2000
+});
+
+$(window).load(function() {
+$('.portfolio_menu ul li').click(function(){
+	$('.portfolio_menu ul li').removeClass('active_prot_menu');
+	$(this).addClass('active_prot_menu');
+});
+
+var $container = $('#portfolio');
+$container.isotope({
+  itemSelector: '.col-sm-4',
+  layoutMode: 'fitRows'
+});
+$('#filters').on( 'click', 'a', function() {
+  var filterValue = $(this).attr('data-filter');
+  $container.isotope({ filter: filterValue });
+  return false;
+});
+});
+
+
+// Check to see if the window is top if not then display button
+$(window).scroll(function(){
+    if ($(this).scrollTop() > 100) {
+        $('.scrolltotop').fadeIn();
+    } else {
+        $('.scrolltotop').fadeOut();
+    }
+});
+
+// Click event to scroll to top
+$('.scrolltotop').click(function(){
+    $('html, body').animate({scrollTop : 0}, 1500, 'easeInOutExpo');
+    return false;
+});
+
+
+$(document).on('click','.navbar-collapse.in',function(e) {
+    if( $(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle' ) {
+        $(this).collapse('hide');
+    }
+});
